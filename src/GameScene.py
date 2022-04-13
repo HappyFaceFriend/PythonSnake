9b2dee1873b6bb01ca063d84432b98900c3ba7a3
HappyFaceFriend <yjl980730@gmail.com>
2022-04-13 15:04:25 +0900

    show score on gamescene

--- src/GameScene.py
+++ src/GameScene.py
@@ -2,23 +2,38 @@ import pygame
 from GameObject import GameObject
 import Input
 import Settings
+import Globals
 from Text import Text
 
-background_color = (100,100,255)
-board_color = (0,255,0)
-board_color2 = (100,255,100)
+background_color = (150,150,255)
+board_color = (50,255,50)
+board_color2 = (150,255,150)
 
 board_rect = pygame.Rect(20, 120, Settings.board_size[0] * Settings.cell_size[0], Settings.board_size[1] * Settings.cell_size[1])
 
 class GameScene:
     def __init__(self):
+        self.crown = GameObject("images/crown.png", Settings.display_width / 2 + 170, 50)
+        self.crown.set_size((self.crown.size[0] / 2, self.crown.size[1] / 2))
+        self.best_score_text = Text(str(Globals.best_score), 30, self.crown.x + 35 , 50)
+        self.score = 0
+        self.score_text = Text("0", 40, Settings.display_width / 2 , 40)
         pass
+
     def update(self, delta_time):
+        if Input.is_key_down(pygame.K_SPACE):
+            self.add_score(1)
         pass
+
     def render(self, gameDisplay):
+        self.render_backgrounds(gameDisplay)
         self.render_UIs(gameDisplay)
-        
-    def render_UIs(self, gameDisplay):
+
+    def add_score(self, score):
+        self.score += score
+        self.score_text.text = str(self.score)
+
+    def render_backgrounds(self, gameDisplay):
         pygame.draw.rect(gameDisplay, background_color, pygame.Rect(0, 0, Settings.display_width, Settings.display_height))
         for i in range(Settings.board_size[1]):
             for j in range(Settings.board_size[0]):
@@ -27,3 +42,7 @@ class GameScene:
                     pygame.draw.rect(gameDisplay, board_color, rect)
                 else:
                     pygame.draw.rect(gameDisplay, board_color2, rect)
+    def render_UIs(self, gameDisplay):
+        self.crown.render(gameDisplay)
+        self.best_score_text.render(gameDisplay)
+        self.score_text.render(gameDisplay)
