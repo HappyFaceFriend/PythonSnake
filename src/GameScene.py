1b1c8cf446e5c2b4f09d7b09abeb36e98c997e90
HappyFaceFriend <yjl980730@gmail.com>
2022-04-13 14:44:52 +0900

    show gamescene map

--- src/GameScene.py
+++ src/GameScene.py
@@ -3,11 +3,27 @@ from GameObject import GameObject
 import Input
 import Settings
 from Text import Text
+
+background_color = (100,100,255)
+board_color = (0,255,0)
+board_color2 = (100,255,100)
+
+board_rect = pygame.Rect(20, 120, Settings.board_size[0] * Settings.cell_size[0], Settings.board_size[1] * Settings.cell_size[1])
+
 class GameScene:
     def __init__(self):
         pass
     def update(self, delta_time):
         pass
     def render(self, gameDisplay):
-        pass
+        self.render_UIs(gameDisplay)
         
+    def render_UIs(self, gameDisplay):
+        pygame.draw.rect(gameDisplay, background_color, pygame.Rect(0, 0, Settings.display_width, Settings.display_height))
+        for i in range(Settings.board_size[1]):
+            for j in range(Settings.board_size[0]):
+                rect = pygame.Rect(board_rect.left + j * Settings.cell_size[0], board_rect.top + i * Settings.cell_size[1], Settings.cell_size[0], Settings.cell_size[1])
+                if (i+j)%2 == 0:
+                    pygame.draw.rect(gameDisplay, board_color, rect)
+                else:
+                    pygame.draw.rect(gameDisplay, board_color2, rect)
--- src/TitleScene.py
+++ src/TitleScene.py
@@ -10,15 +10,15 @@ from GameScene import GameScene
 class TitleScene:
     def __init__(self):
         self.bg = GameObject("images/titlescene_bg.png")
-        self.bg.set_scale((400,400))
+        self.bg.set_size((Settings.display_width, Settings.display_height))
         self.start_button = Button("images/startbutton_default.png", "images/startbutton_hover.png", 
-                                "images/startbutton_click.png", 200, 300)
+                                "images/startbutton_click.png", 160, 300)
 
         self.start_button.onclick = self.button_clicked
 
-        self.crown = GameObject("images/crown.png", 160, 190)
+        self.crown = GameObject("images/crown.png", 120, 190)
         self.speed = 100
-        self.text = Text(text = str(Globals.best_score), size = 40, x = 250, y = 200)
+        self.text = Text(text = str(Globals.best_score), size = 40, x = 210, y = 200)
     
     def button_clicked(self):
         Globals.current_scene = GameScene()
