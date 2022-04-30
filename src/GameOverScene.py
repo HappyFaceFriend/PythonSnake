0781ee04b47ed582136b85acd02346875484b650
oksuman <rlatmdgn77@cau.ac.kr>
2022-05-01 08:52:49 +0900

    gameover scene init

--- src/GameOverScene.py
+++ src/GameOverScene.py
@@ -1,3 +1,5 @@
+from cgitb import text
+from tkinter import CENTER
 import pygame
 from GameObject import GameObject
 import Input
@@ -6,18 +8,25 @@ from Text import Text
 from Button import Button
 import Globals
 
-
-background_color = (150,150,255)
-LIGHTBLUE = (0, 155, 155)
 class GameOverScene:
     def __init__(self, recent_score):
-        self.image_apple = pygame.image.load("images/snake/head.png")
-        pygame.mixer.music.pause()
-        pass
-    
+        pygame.mixer.music.stop()
+
+        self.bg = GameObject("images/gameoverscene/gameoverscene_bg.png")
+        self.bg.set_size((Settings.display_width, Settings.display_height))
+        
+        font = pygame.font.Font("fonts/arial.ttf", 60)
+        self.text = font.render("game over", True, (100,0,0))
+        text_size = self.text.get_rect().size
+        text_width = text_size[0]
+        text_height = text_size[1]
+        self.text_pos = (Settings.display_width/2 - text_width/2, Settings.display_height/4 - text_height/2)
+
     def update(self, delta_time):
         pass
 
     def render(self, gameDisplay):
-        gameDisplay.blit(self.image_apple, (50,50))
-        pass
+        self.bg.render(gameDisplay)
+        gameDisplay.blit(self.text, self.text_pos)
+    
+  
